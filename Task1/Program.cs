fafbea87192ad3f1e6fdbbd2ed1e1026d44bde0d
Vitalii <LVitek2506@yandex.ru>
2023-03-26 12:29:10 +0500

    Первая задача решена

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,15 @@
-﻿
-1
+﻿// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+int ReadInt(string message)
+{
+    Console.Write($"{message} >");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("Введите число");
+int lastDigit = number % 10;
+System.Console.WriteLine($"Последняя цифра числа {number} равна {lastDigit}");
